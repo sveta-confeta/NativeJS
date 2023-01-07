b7fbb200b163dbf62ac709eaf60014299920d676
sveta-confeta <_svet_@tut.by>
2023-01-07 16:54:04 +0300

    каррÑированиеÐ

--- js/train.js
+++ js/train.js
@@ -356,14 +356,89 @@ console.log(sumNum2(5, 8, 9, 11))
 //     }
 // }
 
-function Test (num){
-    let obj={}
-    let sum=Math.sqrt(num)
-    obj['floor']=Math.floor(sum)
-    obj['ceil']=Math.ceil(sum)
-    return obj
+// function Test (num){
+//     let obj={}
+//     let sum=Math.sqrt(num)
+//     obj['floor']=Math.floor(sum)
+//     obj['ceil']=Math.ceil(sum)
+//     return obj
+//
+// }
 
+// console.log(Test(587))
+// function makeWorker() {
+//     let name = "Pete";
+//
+//     return function() {
+//         alert(name);
+//     };
+// }
+//
+// let name = "John";
+//
+// // create a function
+// let work = makeWorker();
+//
+// // call it
+// work();call
+
+
+// Task 01
+// Реализовать функцию sum которая суммирует 2 числа следующим образом sum(3)(6) === 9
+function sum(num){
+    return function (num2){
+        return num + num2
+    }
 }
+console.log(sum(3)(6))
+// Task 02
+// Реализовать функцию makeCounter которая работает следующим образом:
+// const counter = makeCounter();
+// counter(); // 1
+// counter(); // 2
+// const counter2 = makeCounter();
+// counter2(); // 1
+// counter(); // 3
 
-console.log(Test(587))
+// Task 03
+// Переписать функцию из Task 02 так, что бы она принимала число в качестве аргумента и это число было стартовым значением счетчика
+// и возвращала следующий объект методов:
+// increase: +1
+// decrease: -1
+// reset: установить счетчик в 0;
+// set: установить счетчик в заданное значение;
 
+// Task 04*
+// Реализовать функцию superSum которая принимает число в качестве аргумента, которое указывает на количество слагаемых
+// и что бы корректно работали следующие вызовы:
+// 1) superSum(0) //0
+// 2) superSum(3)(2)(5)(3) //10
+// 3) superSum(3)(2)(5,3) //10
+// 4) superSum(3)(2,5,3) //10
+// 5) superSum(3)(2,5)(3) //10
+// 6) superSum(3)(2,5)(3,9) //10
+
+// P.S. типизируйте только аргументы, а при вызове функции используйте @ts-ignore
+
+// Task 05
+// решить все задачи по рекурсии которые даны в конце статьи https://learn.javascript.ru/recursion
+
+// Task 06
+// написать функцию, которая повторяет функционал метода flat массива на всю глубину.
+
+// just a plug
+//export default () => {};
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Closures
+// https://medium.com/@stasonmars/%D0%BF%D0%BE%D0%BD%D0%B8%D0%BC%D0%B0%D0%B5%D0%BC-%D0%B7%D0%B0%D0%BC%D1%8B%D0%BA%D0%B0%D0%BD%D0%B8%D1%8F-%D0%B2-javascript-%D1%80%D0%B0%D0%B7-%D0%B8-%D0%BD%D0%B0%D0%B2%D1%81%D0%B5%D0%B3%D0%B4%D0%B0-c211805b6898
+// https://www.youtube.com/watch?v=pahO5XjnfLA
+
+//// Сurrying
+// https://learn.javascript.ru/currying-partials
+// https://medium.com/@stasonmars/%D0%BF%D0%BE%D0%BD%D0%B8%D0%BC%D0%B0%D0%B5%D0%BC-%D0%BA%D0%B0%D1%80%D1%80%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5-%D0%B2-javascript-5ec4a1d88827
+
+// Pattern Module
+// https://habr.com/ru/company/ruvds/blog/419997/
+
+// Recursion
+// https://learn.javascript.ru/recursion
+// https://www.youtube.com/watch?v=Kuq6oIN3PH0
